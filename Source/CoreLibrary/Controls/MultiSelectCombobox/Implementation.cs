01ae3e4e781a04bfbc2dc1b9cd35bcfff72cc98e
Francois Hoffmann <francoishoffmann1@gmail.com>
2023-04-10 16:40:35 +0200

    Bug fix : Cannot drag and drop in another Richtextbox

--- Source/CoreLibrary/Controls/MultiSelectCombobox/Implementation.cs
+++ Source/CoreLibrary/Controls/MultiSelectCombobox/Implementation.cs
@@ -47,8 +47,9 @@ namespace BlackPearl.Controls.CoreLibrary
             TextPointer textPointer = RichTextBoxElement.GetPositionFromPoint(e.GetPosition(this), true);
             //Check if drag position is different than actual one to avoid blink effect
             int EndOffset = new TextRange(textPointer, RichTextBoxElement.Selection.End).Text.Length;
-            int StartOffset = new TextRange(textPointer, RichTextBoxElement.Selection.End).Text.Length;
-            if (EndOffset == 0 || StartOffset == 0)
+            int StartOffset = new TextRange(textPointer, RichTextBoxElement.Selection.Start).Text.Length;
+            //if EndOffset == 0 && StartOffset == 0 then we are in a another RichTextBox
+            if ((EndOffset == 0 || StartOffset == 0) && !(EndOffset == 0 && StartOffset == 0))
             {
                 e.Effects = DragDropEffects.None;
                 return;
